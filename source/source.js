3253bdd6ecd644db277cf00841c1ef1199e01346
SenhasD <senhasdiwan@gmail.com>
2024-07-06 00:04:17 +0530

    Some works on the gui and implemented a log query function

--- source/source.js
+++ source/source.js
@@ -10,12 +10,28 @@ let favIn;
 let secureTongle = false;
 let advancedMode = false;
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-function advancedModeEnable() {
-	advancedMode = true;
+// function for displaying error and debug messages
+function call(query) {
+	document.getElementById('LOG_text').textContent = query;
+	console.log("DEBUG " + query);
 };
 
+
+// checks if advanced mode enabled or not and enable or disable if required
+function advanced() {
+	if (advancedMode === false) {
+		advancedMode = true;
+		call("Enabled advanced mode");
+	} else if (advancedMode === true) {
+		advancedMode = false;
+		call("Disabled advanced mode");
+	} else {
+		call("Error when enabling or disabling advanced mode");
+	};
+};
+
+// calling with parsing of user enabled settings to the password generating function
 function moreSecure() {
 	while (true) {
 		const opt = prompt("Do you want to make pass more secure? Requires more questions ")
@@ -29,12 +45,19 @@ function moreSecure() {
 			break;
 		} else {
 			alert("Didn't catch that pls try again");
+			call("User has entered bad prompt when enabling secure mode");
 		};
 	};
 };
 
 
-function genPass(username, ageIn, favIn, secureTongle) {	
-
+function genPass(username, ageIn, favIn, secureTongle) {
+	if (secureTongle === true) {
+		call("Loaded options with advanced mode");
+	} else if (secureTongle === false) {
+		call("Loaded options without advanced mode");
+	} else {
+		call("Error during checking if user enabled advanced mode");
+	};
 };
 
